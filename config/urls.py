993210ce143ad176e91658e313612466f2a0efd8
Rise Together <officialrisetogether@gmail.com>
2025-08-19 09:47:48 +0530

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('community.urls')),
+    path('accounts/', include('accounts.urls')),
     path("posts/", include("posts.urls")),
 ]
 if settings.DEBUG:
